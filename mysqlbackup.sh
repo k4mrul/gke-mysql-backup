5c8f12f173ed8ae3f950401384a7e34fda461e35
k4mrul <kamrulahsan06@gmail.com>
2022-10-17 16:47:40 +0600

    enable queue deployment

--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -17,7 +17,7 @@ if [ -z "$BUCKET_NAME" ]; then
 fi
 
 # Path in which to create the backup (will get cleaned later)
-BACKUP_PATH="/mnt/data/dump"
+BACKUP_PATH="/mnt/data/dump/"
 mkdir -p $BACKUP_PATH
 
 # START
@@ -33,20 +33,22 @@ BACKUP_FILENAME="$(get_file_date).tar.gz"
 # Create the backup
 echo "$(get_log_date) [Step 1/3] Running mysqldump"
 
-mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST -N -e 'show databases' | while read dbname; do mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST --complete-insert --routines --triggers --single-transaction "$dbname"  > "$BACKUP_PATH"/"$dbname".sql; done
+mysql -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST -N -e 'show databases' | while read dbname; do mysqldump -uroot -p$MYSQL_ROOT_PASSWORD -h $MYSQL_HOST --complete-insert --routines --triggers --single-transaction "$dbname"  > "$BACKUP_PATH$dbname".sql; done
+
 
 # Compress
 echo "$(get_log_date) [Step 2/3] Creating tar file"
-tar -czf "$BACKUP_PATH/$BACKUP_FILENAME $BACKUP_PATH*"
+tar -czf "$BACKUP_FILENAME" "$BACKUP_PATH"
 
 # Copy to Google Cloud Storage
 echo "$(get_log_date) [Step 3/3] Uploading archive to Google Cloud Storage"
-echo "Copying $BACKUP_PATH/$BACKUP_FILENAME to gs://$BUCKET_NAME/$BACKUP_FILENAME"
-gsutil cp $BACKUP_PATH/$BACKUP_FILENAME gs://$BUCKET_NAME/$FOLDER_NAME/$BACKUP_FILENAME 2>&1
+echo "Copying $BACKUP_FILENAME to gs://$BUCKET_NAME/$BACKUP_FILENAME"
+gsutil cp $BACKUP_FILENAME gs://$BUCKET_NAME/$FOLDER_NAME/$BACKUP_FILENAME 2>&1
 
 # Clean
 echo "Removing backup data"
 rm -rf $BACKUP_PATH*
+rm -f $BACKUP_FILENAME
 
 # FINISH
 echo "$(get_log_date) Copying finished"
